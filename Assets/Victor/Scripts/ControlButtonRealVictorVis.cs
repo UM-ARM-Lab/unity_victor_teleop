c9b35f9d8e81157f3d66f2309ea2a09c9c805ffd
Brad Saund <brad.saund@gmail.com>
2018-10-24 10:01:57 -0400

    Added toggle button for Victor visibility

---
+++ Assets/Victor/Scripts/ControlButtonRealVictorVis.cs
@@ -0,0 +1,37 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using Valve.VR.InteractionSystem;
+
+namespace ArmLab.Victor
+{
+    public class ControlButtonRealVictorVis : MonoBehaviour
+    {
+        public HoverButton hoverButton;
+
+        private bool visible;
+
+        private void Start()
+        {
+            hoverButton.onButtonDown.AddListener(OnButtonDown);
+            visible = true;
+        }
+
+        public void OnButtonDown(Hand fromHand)
+        {
+            visible = !visible;
+            SetVisibility(visible);
+        }
+
+
+        private void SetVisibility(bool vis)
+        {
+            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+            {
+                r.enabled = vis;
+            }
+        }
+    }
+}
